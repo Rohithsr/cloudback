d3af172fa6cfacd0cb8b0fd43934d12fa8689ab9
Rohith <rm0136039@gmail.com>
2024-07-18 20:02:43 +0530

    Initial commit

--- db.js
+++ db.js
@@ -3,10 +3,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'bbhde7knfc2rj4qsfxce-mysql.services.clever-cloud.com', // Replace with your Clever Cloud MySQL host
-    user: 'u5dbrbdazmfsqymm', // Replace with your Clever Cloud MySQL user
-    password: 'kcqCL8kmDFkEVDrB0sax', // Replace with your Clever Cloud MySQL password
-    database: 'bbhde7knfc2rj4qsfxce', // Replace with your Clever Cloud MySQL database name
+    host: 'bippeazoui0cugcsxdd3-mysql.services.clever-cloud.com', // Replace with your Clever Cloud MySQL host
+    user: 'uehnjdy4m4lcnubp', // Replace with your Clever Cloud MySQL user
+    password: 'toU9B1tQCeY1WIU66F6c', // Replace with your Clever Cloud MySQL password
+    database: 'bippeazoui0cugcsxdd3', // Replace with your Clever Cloud MySQL database name
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
